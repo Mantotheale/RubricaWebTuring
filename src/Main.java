9744c61906903001c2d8d7fc9628c3282790ecd3
mantotheale <alessiomantonico@gmail.com>
2025-07-26 19:19:19 +0200

    Le funzionalità di persistenza su database funzionano

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,8 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        Persona persona = new Persona(
-                "Alessio",
-                "Mantonico",
-                "Via Prova X, 12, Milano",
-                "1234567890",
-                23
-        );
-
-        Rubrica rubrica = new Rubrica();
-        rubrica.aggiungiPersona(persona);
+        RubricaDbManager dbManager = new RubricaDbManager("credenziali_database.properties");
+        Rubrica rubrica = new Rubrica(dbManager);
 
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Rubrica");
